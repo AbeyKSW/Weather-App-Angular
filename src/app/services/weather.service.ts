4604ac085740315786e4bbdd6bd61949102b0d2c
Kelum Srimal <kelum.srimal@asiandigitaltech.com>
2021-06-02 13:40:35 +0530

    Caching Mechanism - Test - 2021/06/02

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
-import { tap, map, catchError, finalize } from 'rxjs/operators';
-import { of, throwError } from 'rxjs';
+import { Observable, of, throwError, interval } from 'rxjs';
+import { tap, map, publishReplay, refCount, catchError, finalize } from 'rxjs/operators';
 
 let serviceUrl: String = 'http://api.openweathermap.org/data/2.5/group';
 let cityServiceUrl: String = 'http://api.openweathermap.org/data/2.5/weather';
@@ -14,14 +14,20 @@ let apiKey: String = environment.API_URL;
 })
 export class WeatherService {
 
-  constructor(private http: HttpClient) { }
+  response: any[] = [];
+  apiUrl: string[] = [];
+
+  constructor(private http: HttpClient) {
+    this.clearCache();
+  }
 
   getWeatherData(city_ids: number[]) {
 
-    console.log("Came Here", serviceUrl, city_ids, apiKey);
     // return this.http.get(serviceUrl + '?id=' + city_ids + '&units=metric' + '&APPID=' + apiKey);
-    const http$ = this.http.get(serviceUrl + '?id=' + city_ids + '&units=metric' + '&APPID=' + apiKey);
+    var apiUrl = serviceUrl + '?id=' + city_ids + '&units=metric' + '&APPID=' + apiKey;
+    const http$ = this.http.get(apiUrl);
     return http$;
+
     // http$
     //   .pipe(
     //     map(res => res['payload']),
@@ -43,12 +49,40 @@ export class WeatherService {
     //   );
   }
 
+  // Clear configs
+  clearCache() {
+
+    var clear = setTimeout(() => {
+      this.apiUrl = [];
+      this.response = [];
+    }, 15000);
+  }
+
   getIconUrl(icon: String) {
     return 'http://openweathermap.org/img/w/' + icon + ".png"
   }
 
   getWeatherByCity(city_name: String) {
-    return this.http.get(cityServiceUrl + '?q=' + city_name + '&appid=' + apiKey);
+    // return this.http.get(cityServiceUrl + '?q=' + city_name + '&appid=' + apiKey);
+
+    var response;
+    var apiUrl = cityServiceUrl + '?q=' + city_name + '&appid=' + apiKey;
+    if (this.apiUrl.includes(apiUrl)) {
+      response = this.response.find(x => x.city_name == city_name).response;
+      console.log("Came from Cache", response);
+    }
+    else {
+      response = this.http.get(apiUrl);
+      this.apiUrl.push(apiUrl); // cache url
+      var responseObj = {
+        city_name: city_name,
+        response: response
+      }
+
+      this.response.push(responseObj); // cache data
+      console.log("Came from Server", this.response);
+    }
+    return response;
   }
 }
 
