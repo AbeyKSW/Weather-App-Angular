2b7aa75319a1b7a2f708b9b6042f29696cedd0f6
Kelum Srimal <kelum.srimal@asiandigitaltech.com>
2021-06-03 10:55:08 +0530

    Small fixes - 2021/06/03

--- src/app/directives/random-color.directive.ts
+++ src/app/directives/random-color.directive.ts
@@ -7,6 +7,7 @@ export class RandomColorDirective implements OnInit {
 
   colors: string[] = ['#378DE7', '#6149CB', '#40B681', '#DE934E', '#9C3939'];
   randomItem: string = "";
+  previousColors: string[] = []; // Always have 2 colors
 
   constructor(private element: ElementRef) { }
 
@@ -15,8 +16,26 @@ export class RandomColorDirective implements OnInit {
   }
 
   getRandomColor() {
-    this.randomItem = this.colors[Math.floor(Math.random() * this.colors.length)];
-    console.log('s', this.randomItem);
+    this.randomItem = "";
+    var randomItem = this.colors[Math.floor(Math.random() * this.colors.length)];
+    console.log("Picked Color", randomItem);
+
+    if (this.previousColors.includes(randomItem)) {
+      console.log("Duplicate Color found");
+      this.getRandomColor();
+    }
+    else {
+      console.log("Picked Color is Original");
+      if (this.previousColors.length > 1) {
+        console.log("Color Array Greater than 1");
+        this.previousColors.splice(-1, 1);
+      }
+
+      this.previousColors.push(randomItem);
+      this.randomItem = randomItem;
+      console.log("Color", this.randomItem);
+      console.log("Colors Arr", this.previousColors);
+    }
     return this.randomItem;
   }
 }
